4c9b80d54965a0212115a3f7d12f74be5c3c475a
espensl <espensl@compello.com>
2017-03-03 20:43:14 +0100

    initial commit

---
+++ dcl.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Espen on 03.03.2017.
+ */
+var dcl = {
+    setupScreen: function (width, height, keepSquare, gridScale) {
+        var canvas = document.createElement('canvas');
+        var cols, rows;
+        canvas.id = 'space';
+        canvas.width = width || window.innerWidth;
+        canvas.height = height || window.innerHeight;
+        if(keepSquare){
+            if(canvas.width < canvas.height){
+                canvas.height = canvas.width;
+            } else {
+                canvas.width = canvas.height;
+            }
+        }
+        if(gridScale){
+            cols =  Math.floor(canvas.width / gridScale);
+            rows =  Math.floor(canvas.height / gridScale);
+        } else {
+            cols =  canvas.width;
+            rows =  canvas.height;
+        }
+        document.body.appendChild(canvas);
+
+        return {
+            ctx: canvas.getContext('2d'),
+            width: canvas.width,
+            height: canvas.height,
+            cols: cols,
+            rows: rows,
+            randomSpot: function () {
+                return vector.creteVector(Math.floor(Math.random()* cols), Math.floor(Math.random()*rows));
+            }
+
+        }
+    },
+};
+var vector = {
+  creteVector: function(x, y){
+      return {x: x, y:y};
+  }
+};
