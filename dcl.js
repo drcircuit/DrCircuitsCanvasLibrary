4d2f7a9a4160451c991cf2ddea87ec9945c9f489
espensl <espensl@compello.com>
2017-03-03 23:11:16 +0100

    fixed bower file

--- dcl.js
+++ dcl.js
@@ -1,26 +1,26 @@
 /**
  * Created by Espen on 03.03.2017.
  */
-var dcl = {
+var dcl    = {
     setupScreen: function (width, height, keepSquare, gridScale) {
-        var canvas = document.createElement('canvas');
+        var canvas    = document.createElement('canvas');
         var cols, rows;
-        canvas.id = 'space';
-        canvas.width = width || window.innerWidth;
+        canvas.id     = 'space';
+        canvas.width  = width || window.innerWidth;
         canvas.height = height || window.innerHeight;
-        if(keepSquare){
-            if(canvas.width < canvas.height){
+        if (keepSquare) {
+            if (canvas.width < canvas.height) {
                 canvas.height = canvas.width;
             } else {
                 canvas.width = canvas.height;
             }
         }
-        if(gridScale){
-            cols =  Math.floor(canvas.width / gridScale);
-            rows =  Math.floor(canvas.height / gridScale);
+        if (gridScale) {
+            cols = Math.floor(canvas.width / gridScale);
+            rows = Math.floor(canvas.height / gridScale);
         } else {
-            cols =  canvas.width;
-            rows =  canvas.height;
+            cols = canvas.width;
+            rows = canvas.height;
         }
         document.body.appendChild(canvas);
 
@@ -31,14 +31,20 @@ var dcl = {
             cols: cols,
             rows: rows,
             randomSpot: function () {
-                return vector.creteVector(Math.floor(Math.random()* cols), Math.floor(Math.random()*rows));
+                return vector.creteVector(Math.floor(Math.random() * cols), Math.floor(Math.random() * rows));
             }
 
         }
     },
 };
 var vector = {
-  creteVector: function(x, y){
-      return {x: x, y:y};
-  }
+    creteVector: function (x, y) {
+        return {
+            x: x,
+            y: y,
+            collidesWith: function (vector) {
+                return x === vector.x & y === vector.y;
+            }
+        };
+    }
 };
